426d344914001cfa4bf2594db3fa2bb74017a351
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2025-05-11 12:00:10 -0700

    Imperfect AI adits

--- app.js
+++ app.js
@@ -152,67 +152,8 @@ Slime.getAllInitPack = function(mapNo){
 // Import Player class from separate file
 var Player = require('./serverPlayer.js');
 
-var Bullet = function(parent, angle){
-    var self = Entity();
-    self.id = Math.random();
-    self.spdX = Math.cos(angle/180*Math.PI)* 10;
-    self.spdY = Math.sin(angle/180*Math.PI)* 10;
-    self.parent = parent;
-    self.timer = 0;
-    self.dx = 20;
-    self.dy = 20;
-    self.toRemove = false;
-    self.map = Player.list[parent].map;
-    self.mapNo = Player.list[parent].mapNo;
-    
-    self.update = function(){
-        if(self.timer++ > 100)
-            self.toRemove = true;
-        self.updatePosition();
-        
-        for(var i in Player.list){
-            var p = Player.list[i];
-            if(self.getDistance(p) < 32 && self.parent !== p.id){
-                self.toRemove = true;
-                p.hp -= 1;
-
-                if(p.hp <= 0) {
-                    var shooter = Player.list[self.parent];
-                    if(shooter)
-                        shooter.score += 1;
-                    p.hp = p.hpMax;
-                    p.x = Math.random() * 500;
-                }
-            }
-        }
-    }
-    
-    self.getInitPack = function(){
-        return{
-            id:self.id,
-            x:self.x,
-            y:self.y,
-        };
-    }
-    
-    self.getUpdatePack = function(){
-        return {
-            id:self.id,
-            x:self.x,
-            y:self.y,
-        }
-    }
-    
-    Bullet.list[self.id] = self;
-    for(var i in Map.list){
-        if(Map.list[i].name == self.map){
-            initPack.map[i].bullet.push(self.getInitPack());
-        }
-    }
-    return self;
-}
-
-Bullet.list = {};
+// Import Bullet class from new separate file
+var bulletModule = require('./bullet.js');
 
 var Map = function(data){
 	var self = {};
@@ -286,33 +227,6 @@ Obstacle.getObstacles = function(mapNo){
     return obstacles;
 }
 
-Bullet.getAllInitPack = function(mapNo){
-    var bullets = [];
-    for (var i in Bullet.list){
-        if(Bullet.list[i].mapNo = mapNo){
-            bullets.push(Bullet.list[i].getInitPack());
-        }
-    }
-    return bullets
-}
-
-Bullet.update = function(mapNo){
-    var pack = [];
-    for(var i in Bullet.list){
-        if(Bullet.list[i].map == Map.list[mapNo].name){
-            var bullet = Bullet.list[i];
-            bullet.update();
-            if(bullet.toRemove){
-                delete Bullet.list[i];
-                removePack.map[mapNo].bullet.push(bullet.id);
-            }
-            else
-                pack.push(bullet.getUpdatePack());
-        }
-    }
-    return pack;
-}
-
 var isEmpty = function(mapNo,x,y,dx,dy){
 
 	for(var i in Map.list[mapNo].obstacles){
@@ -369,6 +283,9 @@ for(var i in Map.list){
     initPack.map[i] = {player:[], bullet:[], obstacle:[], slime:[]};
     removePack.map[i] = {player:[], bullet:[], slime:[]};
 }
+
+// Initialize Bullet with required dependencies
+var Bullet = bulletModule(Player.list, Map.list, initPack, removePack);
 
 Slime("test", 900, -200);
 
---
+++ bullet.js
@@ -0,0 +1,104 @@
+var Entity = require('./entity.js');
+
+// Bullet constructor function
+var Bullet = function(parent, angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI)* 10;
+    self.spdY = Math.sin(angle/180*Math.PI)* 10;
+    self.parent = parent;
+    self.timer = 0;
+    self.dx = 20;
+    self.dy = 20;
+    self.toRemove = false;
+    self.map = Player.list[parent].map;
+    self.mapNo = Player.list[parent].mapNo;
+    
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        self.updatePosition();
+        
+        for(var i in Player.list){
+            var p = Player.list[i];
+            if(self.getDistance(p) < 32 && self.parent !== p.id){
+                self.toRemove = true;
+                p.hp -= 1;
+
+                if(p.hp <= 0) {
+                    var shooter = Player.list[self.parent];
+                    if(shooter)
+                        shooter.score += 1;
+                    p.hp = p.hpMax;
+                    p.x = Math.random() * 500;
+                }
+            }
+        }
+    }
+    
+    self.getInitPack = function(){
+        return{
+            id:self.id,
+            x:self.x,
+            y:self.y,
+        };
+    }
+    
+    self.getUpdatePack = function(){
+        return {
+            id:self.id,
+            x:self.x,
+            y:self.y,
+        }
+    }
+    
+    Bullet.list[self.id] = self;
+    for(var i in Map.list){
+        if(Map.list[i].name == self.map){
+            initPack.map[i].bullet.push(self.getInitPack());
+        }
+    }
+    return self;
+}
+
+Bullet.list = {};
+
+Bullet.getAllInitPack = function(mapNo){
+    var bullets = [];
+    for (var i in Bullet.list){
+        if(Bullet.list[i].mapNo = mapNo){
+            bullets.push(Bullet.list[i].getInitPack());
+        }
+    }
+    return bullets;
+}
+
+Bullet.update = function(mapNo){
+    var pack = [];
+    for(var i in Bullet.list){
+        if(Bullet.list[i].map == Map.list[mapNo].name){
+            var bullet = Bullet.list[i];
+            bullet.update();
+            if(bullet.toRemove){
+                delete Bullet.list[i];
+                removePack.map[mapNo].bullet.push(bullet.id);
+            }
+            else
+                pack.push(bullet.getUpdatePack());
+        }
+    }
+    return pack;
+}
+
+// Export the Bullet constructor and methods
+module.exports = function(playerList, mapList, initPack, removePack) {
+    // Store references to required external objects
+    Player = playerList;
+    Map = mapList;
+    
+    // Establish the global variables needed by the Bullet module
+    if (initPack) this.initPack = initPack;
+    if (removePack) this.removePack = removePack;
+    
+    return Bullet;
+};
